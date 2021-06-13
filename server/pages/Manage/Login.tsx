15a2caaefc6a1fcccccb1ba2953d66af7e899867
SeiyaIwabuchi <0123ook@gmail.com>
2021-06-14 02:04:38 +0900

    要らないかもしれない

--- server/pages/Manage/Login.tsx
+++ server/pages/Manage/Login.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps } from "next";
 import querystring from "querystring";
 
 export  const getServerSideProps : GetServerSideProps = async (context) => {
-
     let body = "";
     for(let i=0;i<context.req.readableLength;i++){
         body += context.req.read();
