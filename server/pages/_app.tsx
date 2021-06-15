ae7f90b1fe508f8b18facfddd61a4b77942eb74c
SeiyaIwabuchi <0123ook@gmail.com>
2021-06-16 00:35:29 +0900

    フォントとカラーテーマの設定

---
+++ server/pages/_app.tsx
@@ -0,0 +1,48 @@
+// import App from "next/app";
+import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import type { AppProps /*, AppContext */ } from 'next/app'
+import React from 'react';
+
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Kosugi',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+    palette: {
+      type: 'dark',
+    },
+  });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
+// }
+
+export default MyApp
