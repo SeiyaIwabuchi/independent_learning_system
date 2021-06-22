4e171eddd3ab5b209711ff7a9feecdc111c8723d
SeiyaIwabuchi <0123ook@gmail.com>
2021-06-22 23:13:30 +0900

    fabとハンドラの追加

--- server/components/ManagementCommon.tsx
+++ server/components/ManagementCommon.tsx
@@ -1,6 +1,6 @@
-import { Divider, Drawer, IconButton, ListItem, ListItemText, Menu, MenuItem } from "@material-ui/core";
+import { Divider, Drawer, Fab, IconButton, ListItem, ListItemText, Menu, MenuItem } from "@material-ui/core";
 import { Button, Typography } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import OuterFrame from "./OuterFrame";
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -12,6 +12,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import { ISessionId, SessionId } from "../query_param_shemas/SessionId";
 import ManageTopSwitch from "./ManageTopSwitch";
 import SettingsIcon from '@material-ui/icons/Settings';
+import AddIcon from '@material-ui/icons/Add';
 
 export const LAYOUT_TYPE = {
     EDIT: 1,
@@ -25,13 +26,30 @@ interface IProps {
         state: boolean,
         setState: React.Dispatch<React.SetStateAction<boolean>>,
         msg: string
-    },
-    sessionId : string
-    children?: JSX.Element | JSX.Element[]
+    }
+    sessionId: string
+    AddUrl? : string
+    disableRightButton? : boolean
+    onRightButtonClicked? : (event : any) => any
+    children?: ReactNode
 };
 
 const ManagementCommon = (props: IProps) => {
     const appbar = { title: props.pageTitle };
+    const fab = props.AddUrl != undefined ?
+        (
+        <Fab color="primary" aria-label="add" style={{
+            margin: 0,
+            top: 'auto',
+            right: 20,
+            bottom: 20,
+            left: 'auto',
+            position: 'fixed',
+        }}
+        onClick={()=>{router.push(`${props.AddUrl}?${SessionId({sessionId:props.sessionId})}`)}}>
+            <AddIcon />
+        </Fab>
+        ): (<></>);
     if (props.pageLayoutType == LAYOUT_TYPE.MENU) {
         Object.defineProperty(
             appbar,
@@ -56,7 +74,7 @@ const ManagementCommon = (props: IProps) => {
     } else if (props.pageLayoutType == LAYOUT_TYPE.EDIT) {
         Object.defineProperty(
             appbar,
-            "rightButton",
+            "leftButton",
             {
                 value: (
                     <IconButton onClick={
@@ -67,18 +85,19 @@ const ManagementCommon = (props: IProps) => {
                 )
             }
         );
-        Object.defineProperty(
-            appbar,
-            "leftButton",
-            {
-                value:
-                    (
-                        <IconButton>
-                            <CheckIcon />
-                        </IconButton>
-                    )
-            }
-        );
+        if(!props.disableRightButton)
+            Object.defineProperty(
+                appbar,
+                "rightButton",
+                {
+                    value:
+                        (
+                            <IconButton onClick={props.onRightButtonClicked}>
+                                <CheckIcon />
+                            </IconButton>
+                        )
+                }
+            );
     }
     const isDrawerOpen = useState(false);
     const [snackbarState, setSnackbarState] = useState(false);
@@ -132,7 +151,7 @@ const ManagementCommon = (props: IProps) => {
                 <div style={{ marginTop: "50px" }} />
                 <Divider />
                 <ListItem button key={"goManageTop"}>
-                    <ListItemIcon onClick={() => {router.push(`/Manage/Top?${SessionId({ sessionId: props.sessionId })}`)}}>
+                    <ListItemIcon onClick={() => { router.push(`/Manage/Top?${SessionId({ sessionId: props.sessionId })}`) }}>
                         <SettingsIcon />
                     </ListItemIcon>
                     <ListItemText primary="管理トップ"></ListItemText>
@@ -143,18 +162,19 @@ const ManagementCommon = (props: IProps) => {
                     </ListItemIcon>
                     <ListItemText primary="ログアウト"></ListItemText>
                 </ListItem>
-                </Drawer>
-                <Menu
-                    id="simple-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={() => setAnchorEl(null)}
-                >
-                    <MenuItem onClick={() => setAnchorEl(null)}>ログアウト</MenuItem>
-                </Menu>
-                <ManageTopSwitch isShow={props.sessionId == "Unauthorised"}>
-                    <div
+            </Drawer>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={() => setAnchorEl(null)}
+            >
+                <MenuItem onClick={() => { setAnchorEl(null); onLogout(); }}>ログアウト</MenuItem>
+                <MenuItem onClick={() => { }}>選択項目削除</MenuItem>
+            </Menu>
+            <ManageTopSwitch isShow={props.sessionId == "Unauthorised"}>
+                <div
                     style={{
                         display: "flex",
                         flexDirection: "column",
@@ -166,10 +186,11 @@ const ManagementCommon = (props: IProps) => {
                     <Typography variant="h4" align="center">非公開エリア</Typography>
                     <Typography variant="subtitle1" align="center">{timer}秒後にログイン画面に移動します。</Typography>
                 </div>
-                </ManageTopSwitch>
-                <ManageTopSwitch isShow={props.sessionId != "Unauthorised"}>
-                        {props.children}
-                </ManageTopSwitch>
+            </ManageTopSwitch>
+            <ManageTopSwitch isShow={props.sessionId != "Unauthorised"}>
+                {props.children}
+                {fab}
+            </ManageTopSwitch>
         </OuterFrame>
     )
 };
