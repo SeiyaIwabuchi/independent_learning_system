4da63d740f8b7cd6c037a45250b10382995ed686
SeiyaIwabuchi <0123ook@gmail.com>
2021-06-22 23:14:32 +0900

    material ui チェックボックスがバグる

--- server/components/SubjectMenuList.tsx
+++ server/components/SubjectMenuList.tsx
@@ -1,25 +1,24 @@
 import { ListItem, ListItemText, ListItemSecondaryAction, Divider, List, Checkbox } from "@material-ui/core";
-import React from "react";
+import React, { ReactNode } from "react";
+import { SessionId } from "../query_param_shemas/SessionId";
 
 export interface IElemetPorps{
     primaryText : string,
     secondaryText? : string,
-    destinationURL?: string
+    destinationURL?: string,
+    sessionId : string
 }
 
 const SubjectMenuListElemet = (props : IElemetPorps) => {
     return (
         <>
-            <ListItem button={true} component="a" href={`${props.destinationURL  || "#" }`}>
+            <ListItem button={true} component="a" href={`/Manage/Subject/Edit?${SessionId({sessionId:props.sessionId})}&name=${props.primaryText}`}>
                 <ListItemText
                     primary={props.primaryText}
                     secondary={props.secondaryText || null}
                 />
                 <ListItemSecondaryAction>
-                <Checkbox
-                    edge="end"
-                    color="primary"
-                />
+                    <input type="checkbox" />
                 </ListItemSecondaryAction>
             </ListItem>
         </>
@@ -27,11 +26,16 @@ const SubjectMenuListElemet = (props : IElemetPorps) => {
 };
 
 const SubjectMenuList = (props : {menuList : IElemetPorps[]}) => {
-    const menu : JSX.Element[]= [];
+    const menu : JSX.Element[] = [];
     for(let i=0;i<props.menuList.length;i++){
         let prop = props.menuList[i];
         menu.push(
-            <SubjectMenuListElemet primaryText={prop.primaryText} secondaryText={prop.secondaryText} destinationURL={prop.destinationURL}/>
+            <SubjectMenuListElemet 
+            primaryText={prop.primaryText} 
+            secondaryText={prop.secondaryText} 
+            destinationURL={prop.destinationURL}
+            sessionId={prop.sessionId}
+            />
         );
         if(i < props.menuList.length -1)
             menu.push(<Divider />);
