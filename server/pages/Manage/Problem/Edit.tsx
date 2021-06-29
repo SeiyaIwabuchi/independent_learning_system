297d8d6512cd20b7657dca3854c6deda388cb317
SeiyaIwabuchi <0123ook@gmail.com>
2021-06-30 04:20:53 +0900

    update

--- server/pages/Manage/Problem/Edit.tsx
+++ server/pages/Manage/Problem/Edit.tsx
@@ -2,14 +2,13 @@ import { GetServerSideProps } from "next";
 import React from "react";
 import ManagementCommon, { LAYOUT_TYPE } from "../../../components/ManagementCommon";
 import SessionIdValidater from "../../../utils/SessionIdValidater";
-import { Button, IconButton, List, ListItem, MenuItem } from "@material-ui/core";
+import { Button, Checkbox, IconButton, List, ListItem, MenuItem, Select, TextField } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
-import { Field, Form } from "react-final-form";
-import { Checkboxes, Select, TextField } from "mui-rff";
 import { useState } from "react";
 import { ProblemForm } from "../../../form_schemas/ts/ProblemForm";
 import db, { t_problems } from "../../../models";
+import { FormControl } from "@material-ui/core";
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -28,15 +27,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         },
     }).then(e => {
         choices = []
-        let choicesObj:{[key:number]:any} = {};
-        for(let a of e){
+        let choicesObj: { [key: number]: any } = {};
+        for (let a of e) {
             choicesObj[a.id] = a;
         }
-        let choices_a:number[] = [];
-        for(let n of e) choices_a.push(n.id);
+        let choices_a: number[] = [];
+        for (let n of e) choices_a.push(n.id);
         choices_a.sort((a, b) => a > b ? 1 : -1);
         choices = [];
-        for(let t of choices_a) choices.push(choicesObj[t]);
+        for (let t of choices_a) choices.push(choicesObj[t]);
         choices = JSON.parse(JSON.stringify(choices))
     });
     return {
@@ -64,148 +63,144 @@ interface IProps {
     }[]
 }
 
-const ChoicesList = (
-    props: {
-        choices: [{
-            id: number,
-            choice_text: string,
-            collect_flag: boolean
-        }[],
-            React.Dispatch<React.SetStateAction<{
-                id: number,
-                choice_text: string,
-                collect_flag: boolean
-            }[]>>
-        ]
-    }) => {
-    return (
-        <>
-            {
-                props.choices[0].map((e) => (
-                    <div style={{ display: "flex" }}>
-                        <Checkboxes name={`isCollect_${e.id}`} style={{ flexGrow: 15 }} data={{ label: "", value: e.collect_flag }} />
-                        <TextField name={`choicesText_${e.id}`} style={{ flexGrow: 84, marginBottom: "15px" }} label={`選択肢${e.id}`} variant="outlined" />
-                        <IconButton style={{ flexGrow: 1, marginBottom: "15px" }} onClick={
-                            () => {
-                                props.choices[1](
-                                    props.choices[0].filter(ee => e.id != ee.id)
-                                );
-                            }
-                        }>
-                            <DeleteForeverIcon />
-                        </IconButton>
-                    </div>
-                ))
-            }
-        </>
-    )
-}
-
 const Edit = (props: IProps) => {
-    /*
-    {
-        choicesText_1 : 
+    const [choicesList, setChoicesList]
+        = useState(props.choices);
+    const [problemType, setProblemType]
+        = useState(props.problem.problem_type);
+    const [choiceType, setChoiceType]
+        = useState(props.problem.answer_type);
+    const [problemBody, setProblemBody]
+        = useState(props.problem.problem_body);
+
+    const ChoicesList = () => {
+        const list: JSX.Element[] = [];
+        for (let i = 0; i < choicesList.length; i++) {
+            list.push(
+                <div style={{ display: "flex" }}>
+                    <Checkbox style={{ flexGrow: 15 }} checked={choicesList[i].collect_flag} 
+                    onChange={ (event) => {
+                        const t = choicesList.slice();
+                        t[i].collect_flag = event.target.checked;
+                        setChoicesList(t);
+                        }} />
+                    <TextField name={`choicesText_${i}`} style={{ flexGrow: 84, marginBottom: "15px" }} label={`選択肢${i + 1}`} 
+                    variant="outlined" value={choicesList[i].choice_text} onChange={
+                        (event) => {
+                            const t = choicesList.slice();
+                            t[i].choice_text = event.target.value;
+                            setChoicesList(t);
+                            }}/>
+                    <IconButton style={{ flexGrow: 1, marginBottom: "15px" }} onClick={
+                        () => {
+                            let tlist = choicesList.slice();
+                            tlist.splice(i, 1);
+                            setChoicesList( tlist );
+                        }
+                    }>
+                        <DeleteForeverIcon />
+                    </IconButton>
+                </div>
+            );
+        }
+        return (<>{list}</>);
     }
-    */
-    const choicesObj_:any = {};
-    for(let i=0;i<props.choices.length;i++){
-        choicesObj_[`isCollect_${props.choices[i].id}`] = props.choices[i].collect_flag;
-        choicesObj_[`choicesText_${props.choices[i].id}`] = props.choices[i].choice_text;
-    }
-    const choicesList = useState(props.choices);
+
     return (
         <ManagementCommon
             pageTitle="教科編集"
             pageLayoutType={LAYOUT_TYPE.EDIT}
             sessionId={props.sessionId}
             onRightButtonClicked={() => { }}>
-            <Form
-                onSubmit={async (values, form, callback) => {
+            <form
+                onSubmit={async (e) => {
+                    e.preventDefault();
                     await fetch("/api/Problem", {
                         method: "PUT",
-                        body: JSON.stringify(values)
+                        body: JSON.stringify({
+                            hash: props.problem.hash,
+                            problem_type: problemType,
+                            answer_type: choiceType,
+                            problem_body: problemBody,
+                            choices: choicesList
+                        })
                     }).catch(err => {
                         console.log(err);
                         alert(err);
                     });
                 }}
-                initialValues={{
-                    problem_hash: props.problem.hash,
-                    problemType: props.problem.problem_type,
-                    choiceType: props.problem.answer_type,
-                    problemBody: props.problem.problem_body,
-                    ...choicesObj_
-                }}
-                render={({ handleSubmit, values }) => (
-                    <form
-                        onSubmit={handleSubmit}
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            justifyContent: "space-between",
-                            padding: "10px",
-                        }}>
-                            <Field name="problem_hash" component="input" type="hidden"/>
-                        <Typography variant="body1">問題にはテキストか画像を使用できます。</Typography>
-                        <div style={{ margin: "15px" }}></div>
-                        <Select name="problemType" formControlProps={{ variant: "outlined" }}>
-                            <MenuItem value="0" selected>テキスト</MenuItem>
-                            <MenuItem value="1">画像</MenuItem>
-                        </Select>
-                        <div style={{ margin: "15px" }}></div>
-                        {
-                            values.problemType == "0" ?
-                                (<TextField name="problemBody" variant="outlined" label="問題文" multiline />) :
-                                (
-                                    <>
-                                        <Button variant="contained" color="primary">画像追加</Button>
-                                        <div style={{ margin: "15px" }}></div>
-                                        <img src={"http://via.placeholder.com/500x300"}></img>
-                                    </>
-                                )
-                        }
-                        <div style={{ margin: "15px" }}></div>
-                        <Typography variant="body1">回答方法には選択式とテキスト入力が使えます。</Typography>
-                        <div style={{ margin: "15px" }}></div>
-                        <Select name="choiceType" formControlProps={{ variant: "outlined" }} >
-                            <MenuItem value="">
-                                <em>未選択</em>
-                            </MenuItem>
-                            <MenuItem value="0">選択式</MenuItem>
-                            <MenuItem value="1">テキスト</MenuItem>
-                        </Select>
-                        <div style={{ margin: "15px" }}></div>
-                        {values.choiceType == "0" ? (
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    padding: "10px",
+                }}>
+                <Typography variant="body1">問題にはテキストか画像を使用できます。</Typography>
+                <div style={{ margin: "15px" }}></div>
+                <FormControl>
+                    <Select name="problemType" variant="outlined" value={problemType} 
+                    onChange={event => setProblemType(event.target.value as number)}>
+                        <MenuItem value={0} selected>テキスト</MenuItem>
+                        <MenuItem value={1} >画像</MenuItem>
+                    </Select>
+                </FormControl>
+                <div style={{ margin: "15px" }}></div>
+                {
+                    problemType == 0 ?
+                        (<TextField name="problemBody" variant="outlined" label="問題文"
+                            value={problemBody} multiline onChange={(event) => setProblemBody(event.target.value)} />) :
+                        (
                             <>
-                                <Typography variant="body1">回答用の選択肢</Typography>
+                                <Button variant="contained" color="primary">画像追加</Button>
                                 <div style={{ margin: "15px" }}></div>
-                                <div style={{ display: "flex" }}>
-                                    <Typography variant="body1" style={{ flexGrow: 1, textAlign: "center" }}>答え</Typography>
-                                    <Typography variant="body1" style={{ flexGrow: 16, textAlign: "center" }}>選択肢</Typography>
-                                    <Typography variant="body1" style={{ flexGrow: 1, textAlign: "center" }}>削除</Typography>
-                                </div>
-                                <List>
-                                    <ChoicesList choices={choicesList} />
-                                </List>
-                                <Button variant="contained" color="primary" onClick={() => {
-                                    const tlist = choicesList[0].slice();
-                                    tlist.push({
-                                        id: Math.max(...choicesList[0].map(e => e.id)) + 1,
-                                        collect_flag: false,
-                                        choice_text: ""
-                                    });
-                                    choicesList[1](tlist);
-                                }
-                                }>選択肢追加</Button>
+                                <img src={"http://via.placeholder.com/500x300"}></img>
                             </>
-                        ) : (
-                            <TextField name="collectText" variant="outlined" label="正解のテキスト"></TextField>
-                        )}
+                        )
+                }
+                <div style={{ margin: "15px" }}></div>
+                <Typography variant="body1">回答方法には選択式とテキスト入力が使えます。</Typography>
+                <div style={{ margin: "15px" }}></div>
+                <FormControl variant="outlined">
+                    <Select name="choiceType" value={choiceType} 
+                    onChange={event => setChoiceType(event.target.value as number)}>
+                        <MenuItem value={0}>選択式</MenuItem>
+                        <MenuItem value={1}>テキスト</MenuItem>
+                    </Select>
+                </FormControl>
+                <div style={{ margin: "15px" }}></div>
+                {choiceType == 0 ? (
+                    <>
+                        <Typography variant="body1">回答用の選択肢</Typography>
                         <div style={{ margin: "15px" }}></div>
-                        <Button variant="contained" color="secondary" type="submit">問題追加</Button>
-                    </form>
+                        <div style={{ display: "flex" }}>
+                            <Typography variant="body1" style={{ flexGrow: 1, textAlign: "center" }}>答え</Typography>
+                            <Typography variant="body1" style={{ flexGrow: 16, textAlign: "center" }}>選択肢</Typography>
+                            <Typography variant="body1" style={{ flexGrow: 1, textAlign: "center" }}>削除</Typography>
+                        </div>
+                        <List>
+                            <ChoicesList />
+                        </List>
+                        <Button variant="contained" color="primary" onClick={() => {
+                            const tlist = choicesList.slice();
+                            tlist.push({
+                                id: Math.max(...choicesList.map(e => e.id)) + 1,
+                                collect_flag: false,
+                                choice_text: ""
+                            });
+                            setChoicesList(tlist);
+                        }
+                        }>選択肢追加</Button>
+                    </>
+                ) : (
+                    <TextField name="collectText" variant="outlined" label="正解のテキスト"></TextField>
                 )}
-            />
+                <div style={{ margin: "15px" }}></div>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    type="submit"
+                    >問題追加</Button>
+            </form>
         </ManagementCommon>
     )
 };
--- server/pages/api/Problem.ts
+++ server/pages/api/Problem.ts
@@ -1,48 +1,69 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { UserForm } from "../../form_schemas/ts/UserForm";
 import db from "../../models";
 import SessionIdValidater from "../../utils/SessionIdValidater";
-import crypto from "crypto";
 import { Op } from "sequelize";
 
 type problem_UPDATE = {
     [key: string]:any;
-    'problem_hash'? : string;
-    'problemType'? : number;
-    'choiceType'? : number;
-    'problemBody'? : string;
-    'choices' : {isCollect?: boolean,choiceText?: string}[]
+    'hash'? : string;
+    'problem_type'? : number;
+    'answer_type'? : number;
+    'problem_body'? : string;
+    'choices' : {
+        id: number,
+        problem_id: number,
+        choice_text: string,
+        collect_flag: boolean,
+        image_id: string,
+    }[]
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if(await SessionIdValidater(undefined,req.cookies.sessionId) != `Unauthorised`){
-        const tproblem = JSON.parse(req.body);
-        const re_isCollect_x = /isCollect_(\d+)/;
-        const re_choicesText_X = /choicesText_(\d+)/;
-        const problem: problem_UPDATE = {choices:[]};
-        const tchoices: {collect:any[],choiceText:any[]} = {collect:[],choiceText:[]};
-        for(let key of Object.keys(tproblem)){
-            let matchResult = key.match(re_isCollect_x);
-            if(matchResult != null){
-                tchoices.collect.push(tproblem[key]);
-            }
-            matchResult = key.match(re_choicesText_X);
-            if(matchResult != null){
-                tchoices.choiceText.push(tproblem[key]);
-            }
-            problem[key] = tproblem[key];
-        }
-        console.log(problem);
-        console.log(tchoices);
+        const problem: problem_UPDATE = JSON.parse(req.body);
         if(req.method == "POST"){
         }else if(req.method == "PUT"){
             // update t_problem
-            // db.t_problems.findOne({
-            //     where : {
-            //         hash: 
-            //     }
-            // })
+            let problemId:number;
+            await db.t_problems.findOne({
+                where : {
+                    hash: problem.hash!
+                }
+            })
+            .then(e => {
+                e!.problem_body = problem.problem_body!;
+                e!.problem_type = problem.problem_type!;
+                e!.answer_type = problem.answer_type!;
+                e!.save();
+                problemId = e!.id;
+            });
+            for(let e of problem.choices){
+                await db.t_choices.findOne({
+                    where: {id: e.id}
+                })
+                .then(async rec => {
+                    if(rec){
+                        rec.choice_text = e.choice_text;
+                        rec.collect_flag = e.collect_flag;
+                        rec.save();
+                    }else{
+                        await db.t_choices.create({
+                            choice_text: e.choice_text,
+                            collect_flag: e.collect_flag
+                        });
+                    }
+                })
+            }
+            const choiceIdList = problem.choices.map(r => r.id);
+            await db.t_choices.destroy({
+                where: {
+                    id: {
+                        [Op.notIn]: choiceIdList
+                    },
+                    problem_id: problemId!
+                }
+            });
         }else if(req.method == "DELETE"){
         }
         res.json({message:"dummy"});
