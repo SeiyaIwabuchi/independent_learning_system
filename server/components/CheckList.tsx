380e94111a9655afbe637a57d099fb2a54c76615
SeiyaIwabuchi <0123ook@gmail.com>
2022-05-12 15:52:43 +0900

    チェックリストで画像を表示できるようした

--- server/components/CheckList.tsx
+++ server/components/CheckList.tsx
@@ -5,8 +5,13 @@ import { dexieDb, IMarkList } from "../models/dexie";
 const CheckList = (props: { checkedList: IMarkList }) => {
     const [checked, setChecked] = useState(true);
     return (
-        <ListItem>
-            <ListItemText primary={props.checkedList.problem_body} />
+        <ListItem key={props.checkedList.hash}>
+            {
+                props.checkedList.problem_type == 0 ?
+                <ListItemText primary={props.checkedList.problem_body} />
+                :
+                <img src={props.checkedList.problem_image_url || "http://via.placeholder.com/500x300"} width="90%" height="auto"></img>
+            }
             <ListItemSecondaryAction>
                 <Checkbox
                     checked={checked}
